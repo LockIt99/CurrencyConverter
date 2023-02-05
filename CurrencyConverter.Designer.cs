16d3e9b64e15985a6e363fecdd35002884df0b29
LockIt99 <kishorekummitha@gmail.com>
2023-02-05 13:31:39 +0530

    ID#5106 - CurrencyConverter - Suppress script errors

---
+++ CurrencyConverter.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace CurrencyConverter
+{
+    partial class CurrencyConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCurrencyConv = new System.Windows.Forms.Button();
+            this.wbBody = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // btnCurrencyConv
+            // 
+            this.btnCurrencyConv.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btnCurrencyConv.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCurrencyConv.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCurrencyConv.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCurrencyConv.Location = new System.Drawing.Point(0, 0);
+            this.btnCurrencyConv.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCurrencyConv.Name = "btnCurrencyConv";
+            this.btnCurrencyConv.Size = new System.Drawing.Size(806, 43);
+            this.btnCurrencyConv.TabIndex = 0;
+            this.btnCurrencyConv.Text = "Currency Converter";
+            this.btnCurrencyConv.UseVisualStyleBackColor = false;
+            this.btnCurrencyConv.Click += new System.EventHandler(this.btnCurrencyConv_Click);
+            // 
+            // wbBody
+            // 
+            this.wbBody.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.wbBody.Location = new System.Drawing.Point(0, 47);
+            this.wbBody.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.wbBody.MinimumSize = new System.Drawing.Size(13, 13);
+            this.wbBody.Name = "wbBody";
+            this.wbBody.ScriptErrorsSuppressed = true;
+            this.wbBody.Size = new System.Drawing.Size(806, 368);
+            this.wbBody.TabIndex = 1;
+            // 
+            // CurrencyConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 415);
+            this.Controls.Add(this.wbBody);
+            this.Controls.Add(this.btnCurrencyConv);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CurrencyConverter";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Currency Converter";
+            this.Load += new System.EventHandler(this.CurrencyConverter_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCurrencyConv;
+        private System.Windows.Forms.WebBrowser wbBody;
+    }
+}
+
---
+++ CurrencyConverter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace CurrencyConverter
+{
+    public partial class CurrencyConverter : Form
+    {
+        public CurrencyConverter()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCurrencyConv_Click(object sender, EventArgs e)
+        {
+            wbBody.Navigate("https://www.exchange-rates.org/converter/");
+        }
+
+        private void CurrencyConverter_Load(object sender, EventArgs e)
+        {
+            string appName = Process.GetCurrentProcess().ProcessName + ".exe";
+            SetIEkeyBrowserContrl(appName);
+            wbBody.Navigate("https://www.exchange-rates.org/converter/");
+        }
+        static void SetIEkeyBrowserContrl(string appName)
+        {
+            RegistryKey regKey = null;
+            try
+            {
+                if (Environment.Is64BitOperatingSystem)
+                    regKey = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(@"SOFTWARE\\Wow6432Node\\Microsoft\\Internet Explorer\\Main\\FeatureControl\\FEATURE_BROWSER_EMULATION", true);
+                else
+                    regKey = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(@"SOFTWARE\\Microsoft\\Internet Explorer\\Main\\FeatureControl\\FEATURE_BROWSER_EMULATION", true);
+                if (regKey == null)
+                {
+                    MessageBox.Show("Failed to save settings. Address not found");
+                    return;
+                }
+                int key = Convert.ToInt32( regKey.GetValue(appName));
+                if (key <= 0)
+                    regKey.SetValue(appName, unchecked((int)0x2AF8), RegistryValueKind.DWord);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Occured while doing reg setup -  " + ex.Message);
+            }
+            finally
+            {
+                if (regKey != null) regKey.Close();
+            }
+        }
+
+    }
+}
